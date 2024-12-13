a3e317a8de9052b06a0443855a6dbd43f0b3b83e
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-12-13 14:00:23 +0000

    test: :test_tube: add shiny app protocol driver and first dsl verb implementation

--- tests/acceptance/dsl.R
+++ tests/acceptance/dsl.R
@@ -1,10 +1,47 @@
 box::use(
+  R6[R6Class],
+  rhino,
+  selenider,
+  shinytest2,
   testthat[...],
+  withr[with_dir],
 )
+
+AppDriver <- R6Class(
+  public = list(
+    session = NULL,
+    driver = NULL,
+    initialize = function(app = rhino$app()) {
+      with_dir("../../", {
+        self$driver <- shinytest2$AppDriver$new(app)
+        self$session <- selenider$selenider_session(
+          driver = self$driver,
+          local = FALSE
+        )
+      })
+    },
+    record_income = function(amount, driver) {
+      self$session |>
+        selenider$find_element("[data-test='income']") |>
+        selenider$elem_set_value(amount)
+    }
+  )
+)
+
+get_driver_factory <- function() {
+  driver <- NULL
+  function() {
+    if (is.null(driver)) {
+      driver <<- AppDriver$new()
+    }
+  }
+}
+get_driver <- get_driver_factory()
 
 #' @export
 record_income <- function(amount) {
-
+  driver <- get_driver()
+  driver$record_income(amount)
 }
 
 #' @export
